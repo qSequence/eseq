ce33dd86847f44bbfd0a9abc097cc99fce28705a
Anirban <anirban0031@gmail.com>
2018-04-04 23:34:46 +0600

    A

--- WebContent/javascript/anirbanJS.js
+++ WebContent/javascript/anirbanJS.js
@@ -4,3 +4,10 @@ $(document).ready(function(){
   var winPaddTop= winHeight/4;
 	$(".home-cont").css("margin-top",winPaddTop);
 });
+
+$(document).ready(function(){
+
+	var winHeight = $(window).height();
+  var winPaddTop= winHeight/10;
+	$(".loginCenter").css("margin-top",winPaddTop);
+});
